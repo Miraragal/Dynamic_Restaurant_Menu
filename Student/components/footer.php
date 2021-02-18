a6321995f3b9237fc410386c900a812ce12027fb
Miriam <miraragal@gmail.com>
2021-02-18 16:57:33 -0700

    Home

---
+++ Student/components/footer.php
@@ -0,0 +1,28 @@
+<div id='footer' class='cf'>
+    <div class="column three">
+        <strong>Phone</strong>
+        718-546-9876
+    </div>
+    <div class="column three">
+        <strong>Localition</strong>
+        123 Kapiolani Boulevard<br>
+        Colorado Springs, CO
+    </div>
+
+    <div class="column three last">
+        <strong>Hours</strong>
+        <em>Tuesday- Thursday</em><br>
+        8:00am - 9:00pm<br><br>
+        <em>Friday- Sunday</em><br>
+        8:00am - 11:00pm<br><br>
+        <em>Monday</em><br>
+        Close<br><br>
+    </div>
+    <!--footer-->
+    <small>&copy; <?php echo date('Y'); echo " "."Miriam Aragon" ?></small>
+</div> <!-- content -->
+
+</div><!-- wrapper -->
+
+</body>
+</html>
---
+++ Student/components/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP <?php echo TITLE; ?></title>
+    <link href="../assets/styles.css" rel="stylesheet">
+</head>
+
+<body id="final-example">
+    <div class='wrapper'>
+
+        <div id='banner'>
+            <a href='/' title="Return to Home">
+                <img src="img/banner.jpg" alt="Milo's Fine Dinning" />
+            </a>
+        </div><!-- banner -->
+
+        <div id='nav'>
+            <ul>
+                <li><a href='index.php'>Home</a></li>
+                <li><a href='team.php'>Team</a></li>
+                <li><a href='menu.php'>Menu</a></li>
+                <li><a href='contact.php'>Contact</a></li>
+            </ul>
+        </div> <!-- nav -->
+        <div class='content'>
+
+
+
+
+
+
+
